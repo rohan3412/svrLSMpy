8bd4f27e6f120e2ed2dfcd44a736bf57cb142ee0
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2025-06-18 22:48:17 +0530

    Update load_lesions_and_behaviors.py

--- svrLSMpy/load_lesions_and_behaviors.py
+++ svrLSMpy/load_lesions_and_behaviors.py
@@ -3,7 +3,8 @@ from pandas import read_csv
 import numpy as np
 import nibabel as nib
 import os
-def load_lesions_and_behaviors(lesion_folder, csv_file, max_score, do_regress_out_lesion_volume=False):
+
+def load_lesions_and_behaviors(lesion_folder, csv_file, max_score, do_regress_out_lesion_volume, do_regress_out_covariates):
     """
     Load lesion files, behavioral data, and covariates from CSV, and compute lesion volumes.
     """
@@ -37,21 +38,35 @@ def load_lesions_and_behaviors(lesion_folder, csv_file, max_score, do_regress_ou
     lesion_volumes = np.array(lesion_volumes).reshape(-1, 1)
 
     # Load additional covariates
-    covariates = df.iloc[:, 2:].values  # Assuming additional covariates start from the 3rd column
+    covariates = df.drop(columns=["filename", "behavior"])
+    covariates = covariates.select_dtypes(include=["number"])
+
+    if covariates.isna().any().any():
+        covariates = covariates.fillna(covariates.mean())
+        print("Filled NaN values in covariates with column means.")
+
     if covariates.shape[1] > 0:
-        print(f"Loaded {covariates.shape[1]} additional covariates",end='')
-        # Combine lesion volumes with additional covariates
-        if do_regress_out_lesion_volume:
-            print(" and lesion volume as covariate")
-            covariates = np.hstack([lesion_volumes, covariates])
-        else:
-            print("\nLesion volume not regressed out as covariate")
-            covariates = np.hstack([covariates])
-        # Z-transform covariates
-        scaler = StandardScaler()
-        covariates = scaler.fit_transform(covariates)
+        if do_regress_out_covariates:
+            print(f"Loaded {covariates.shape[1]} additional covariates",end='')
+            # Combine lesion volumes with additional covariates
+            if do_regress_out_lesion_volume:
+                print(" and lesion volume as covariate")
+                covariates = np.hstack([lesion_volumes, covariates])
+            else:
+                print("\nand lesion volume not regressed out as covariate")
+                covariates = np.hstack([covariates])
+
+    elif do_regress_out_lesion_volume:
+            print("Loaded lesion volume only as covariate")
+            covariates = np.hstack([lesion_volumes])
     else:
         print("No additional covariates found in the CSV.")
         covariates = None
+
+    if covariates is not None:
+        # Z-transform covariates
+        scaler = StandardScaler()
+
+        covariates = scaler.fit_transform(covariates)
 
     return lesion_files, behaviors, covariates, lesion_volumes
