0212fbab2c003378a135736effe66f828d9b1355
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2025-06-18 23:31:28 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,32 +1,8 @@
-# svrLSMpy/examples.py (or svrLSMpy/main.py)
-from pathlib import Path
-from .run_svr_lsm_iteration import run_svr_lsm_iteration
+from svrLSMpy import run_svr_lsm_iteration
 
-def run_example():
-    base_folder = Path.cwd() / "symptoms"
-    output_path = Path.cwd() / "output"
-    symptom_folder = base_folder / 'VAST'
-    csv_name = "VAST_Data_Fluency.csv"
-
-    param_grid = {
-        'C': [50],
-        'gamma': [2],
-        'epsilon': [0.1]
-    }
-
-    behaviour_name = "Aphasia"
-    run_svr_lsm_iteration(
-        symptom_folder=symptom_folder,
-        csv_name=csv_name,
-        output_path=output_path,
-        behaviour_name=behaviour_name,
-        normalize_vector=True,
-        do_regress_out_lesion_volume=True,
-        max_score=4,
-        min_patient_count="10%",
-        param_grid=param_grid,
-        n_permutations=1,
-        alpha=0.05,
-        n_splits=2,
-        num_slices=7
-    )
+run_svr_lsm_iteration(
+    symptom_folder="proj/data-final",
+    csv_path="proj/data-no-covariates.csv",
+    max_score=100,
+    output_path="test-package-output"
+)
