29fc6d5a25b50e4a4bc23fcf4f67fbf192bcd7f3
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2025-10-21 15:57:13 +0530

    Update run_svr_lsm_iteration.py

--- svrLSMpy/run_svr_lsm_iteration.py
+++ svrLSMpy/run_svr_lsm_iteration.py
@@ -25,7 +25,7 @@ def run_svr_lsm_iteration(symptom_folder,
                           param_grid = {
                                         'C': [50, 40, 30, 20, 10, 5, 1],
                                         'gamma': [50, 10, 5, 4, 3, 2, 1, 'scale', 'auto'],
-                                        'epsilon': [0.1, 0.05, 0.01]
+                                        'epsilon': [0.1]
                                         },
                           n_permutations=1000, 
                           alpha=0.05, 
@@ -133,6 +133,7 @@ def run_svr_lsm_iteration(symptom_folder,
                 normalize_vector)
 
     print("\n\tTOTAL TIME TAKEN : ", easy_time(int(time.time() - start_time)))
+
 
 
 
