b0a7bad928421e58f77e3e4e472e77ccfadcac82
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2025-10-27 14:28:08 +0530

    Update run_svr_lsm_iteration.py

--- svrLSMpy/run_svr_lsm_iteration.py
+++ svrLSMpy/run_svr_lsm_iteration.py
@@ -54,7 +54,9 @@ def run_svr_lsm_iteration(symptom_folder,
     # Load lesions and behaviors
     lesion_folder = Path(symptom_folder)
 
-    lesion_files, behaviors, covariates, lesion_volumes = load_lesions_and_behaviors(lesion_folder, csv_path, max_score, regress_out_lesion_volume, regress_out_covariates_on_scores)
+    do_regress_out_covariates = regress_out_covariates_on_scores or regress_out_covariates_on_lesions
+                            
+    lesion_files, behaviors, covariates, lesion_volumes = load_lesions_and_behaviors(lesion_folder, csv_path, max_score, regress_out_lesion_volume, do_regress_out_covariates)
     print("\n\tTIME ELAPSED : ", easy_time(int(time.time() - start_time)),end="\n\n")
 
     min_patient_count, features, masker = filter_voxels_by_patient_count(lesion_files, min_patient_count, normalize_vector, output_folder)
@@ -139,6 +141,7 @@ def run_svr_lsm_iteration(symptom_folder,
                 normalize_vector)
 
     print("\n\tTOTAL TIME TAKEN : ", easy_time(int(time.time() - start_time)))
+
 
 
 
